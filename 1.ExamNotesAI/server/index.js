4909d11d79fbabb3239f8cfa8fca14bff7569a0d
Manshikatiyar <nishukatiyar97@gmail.com>
2026-04-16 15:55:01 +0530

    Update index.js

--- 1.ExamNotesAI/server/index.js
+++ 1.ExamNotesAI/server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://one-examnotesai-3-pwqw.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
