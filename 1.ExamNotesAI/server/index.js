bbf2198db53303247c19c5c88fd696daa7efce1c
Manshikatiyar <nishukatiyar97@gmail.com>
2026-07-02 01:22:12 +0530

    Fix CORS and cookie
    :wq#:wq

--- 1.ExamNotesAI/server/index.js
+++ 1.ExamNotesAI/server/index.js
@@ -1,20 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDb from "./utils/connectDb.js"
-import authRouter from "./routes/auth.route.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import userRouter from "./routes/user.route.js"
-import notesRouter from "./routes/genrate.route.js"
-import pdfRouter from "./routes/pdf.route.js"
-import creditRouter from "./routes/credits.route.js"
-import { stripeWebhook } from "./controllers/credits.controller.js"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./utils/connectDb.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRouter from "./routes/user.route.js";
+import notesRouter from "./routes/genrate.route.js";
+import pdfRouter from "./routes/pdf.route.js";
+import creditRouter from "./routes/credits.route.js";
+import { stripeWebhook } from "./controllers/credits.controller.js";
 
+dotenv.config();
 
-
-
-const app = express()
+const app = express();
 
 app.post(
   "/api/credits/webhook",
@@ -22,31 +20,29 @@ app.post(
   stripeWebhook
 );
 
-app.use(cors(
-    {origin:"https://one-examnotesai-3-pwqw.onrender.com",
-        credentials:true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-))
 
+app.use(cors({
+    origin: "https://one-examnotesai-3-pwqw.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
-const PORT = process.env.PORT || 5000
-app.get("/",(req,res)=>{
-    res.json({message:"ExamNotes AI Backend Running 🚀"})
+const PORT = process.env.PORT || 5000;
 
-})
-app.use("/api/auth" , authRouter)
-app.use("/api/user", userRouter)
-app.use("/api/notes", notesRouter)
-app.use("/api/pdf", pdfRouter)
-app.use("/api/credit",creditRouter)
+app.get("/", (req, res) => {
+    res.json({ message: "ExamNotes AI Backend Running 🚀" });
+});
 
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/pdf", pdfRouter);
+app.use("/api/credit", creditRouter);
 
-
-app.listen(PORT,()=>{
-    console.log(`✅ Server running on port ${PORT}`)
-    connectDb()
-})
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+    connectDb();
+});
